4367f37bf93c2f1844d7e3b64bb97be403e481b7
Taha <taha.naseem589@gmail.com>
2023-12-14 01:15:34 +0500

    Inheriting and overriding the properties of Object

--- inheritance.js
+++ inheritance.js
@@ -1,7 +1,16 @@
 var bird = {
-    hasWingS: true,
+    hasWings: true,
     canFly: true,
-    hasFeatures: true
+    hasFeathers: true
 }
 var eagle1 = Object.create(bird);
-console.log("eagle1: ",eagle1);
+console.log("Eagle1:",eagle1);
+console.log("Eagle1 has wings:",eagle1.hasWings);
+console.log("Eagle1 can fly:",eagle1.canFly);
+console.log("Eagle1 has feathers:",eagle1.hasFeathers);
+
+var eagle2 = Object.create(bird);
+console.log("Eagle2 has wings:",eagle2.hasWings);
+var penguin1 = Object.create(bird);
+penguin1.canFly = false;
+console.log("Penguin1 can fly:",penguin1.canFly);
