ed18abb61426c148d59cd806647ce2b84df049f1
Taha <taha.naseem589@gmail.com>
2023-12-14 01:55:21 +0500

    Created a sub-class

--- classes.js
+++ classes.js
@@ -16,10 +16,32 @@ class Train {
         var proto = Object.getPrototypeOf(this);
         console.log(proto);
     }
-}
+};
 var obj = new Train("Red",false);
-obj.lightsStatus();
-obj.toggleLights();
-obj.lightsStatus();
-obj.getSelf();
-obj.getPrototype();
+// obj.lightsStatus();
+// obj.toggleLights();
+// obj.lightsStatus();
+// obj.getSelf();
+// obj.getPrototype();
+
+class HighSpeedTrain extends Train {
+    constructor(passengers, highSpeedOn, color, lightsOn) {
+        super(color, lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+    toggleHighSpeed(){
+        this.highSpeedOn =!this.highSpeedOn;
+        console.log("High Speed Status is:",this.highSpeedOn);
+    }
+    toggleLights(){
+        super.toggleLights();
+        super.lightsStatus();
+        console.log("Lights are 100% operational");
+    }
+}
+
+var obj2 = new HighSpeedTrain(200,false,"blue",false);
+obj2.toggleHighSpeed();
+obj2.toggleLights();
+obj2.getSelf();
