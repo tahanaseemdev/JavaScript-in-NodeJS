a3c1be57327ba334d7742649c6b5d3a62a71c5bf
Taha <taha.naseem589@gmail.com>
2023-12-15 00:08:34 +0500

    Object.keys(), Object.values(), and Object.entries()

--- notes.js
+++ notes.js
@@ -180,20 +180,20 @@
 //     console.log(color);
 // }
 
-const car2 = {
-    speed: 200,
-    color: "red"
-}
-console.log(Object.keys(car2)); // ['speed','color']
+// const car2 = {
+//     speed: 200,
+//     color: "red"
+// }
+// console.log(Object.keys(car2)); // ['speed','color']
 
-const car3 = {
-    speed: 300,
-    color: "yellow"
-}
-console.log(Object.values(car3)); // [300, 'yellow']
+// const car3 = {
+//     speed: 300,
+//     color: "yellow"
+// }
+// console.log(Object.values(car3)); // [300, 'yellow']
 
-const car4 = {
-    speed: 400,
-    color: 'magenta'
-}
-console.log(Object.entries(car4)); // [ ['speed', 400], ['color', 'magenta'] ]
+// const car4 = {
+//     speed: 400,
+//     color: 'magenta'
+// }
+// console.log(Object.entries(car4)); // [ ['speed', 400], ['color', 'magenta'] ]
